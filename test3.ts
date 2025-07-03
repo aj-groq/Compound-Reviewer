6145e7cee168d3255635d6b46de25d45ec9471ab
Arnav Jaiswal <ajaiswal@groq.com>
2025-07-02 17:30:52 -0700

    new

---
+++ test3.ts
@@ -0,0 +1,12 @@
+function add(a: number, b: number) {
+  return a + b;
+}
+
+function multiply(x: number, y: number) {
+  return x * y;
+}
+function subtract(a: number, b: number) {
+  return a - b;
+}
+
+export default { add, multiply, subtract }; 
