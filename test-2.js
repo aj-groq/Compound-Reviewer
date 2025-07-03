e2815c5090e813d652fa5784092b314604094bad
Arnav Jaiswal <ajaiswal@groq.com>
2025-07-02 17:08:24 -0700

    test 2

---
+++ test-2.js
@@ -0,0 +1,13 @@
+function add(a, b) {
+  return a - b;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+function subtract(a, b) {
+  return a / b;
+}
+
+
+export default { add, multiply, subtract }; 
