1b01e63afc94e7c9a6a8ad853b4810d64e3eea4c
Arnav Jaiswal <ajaiswal@groq.com>
2025-07-02 17:26:43 -0700

    test3

---
+++ test3.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+  return a - b;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+function subtract(a, b) {
+  return a - c;
+}
+
+export default { add, multiply, subtract }; 
