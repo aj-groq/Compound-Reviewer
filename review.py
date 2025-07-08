226549ac4c6b24485c467bcee61f628bcc4288c1
Arnav Jaiswal <ajaiswal@groq.com>
2025-07-07 19:46:51 -0700

    test

--- review.py
+++ review.py
@@ -2,11 +2,6 @@ def calculate_median(numbers: list[int | float]) -> float:
     """Calculate the median of a list of numbers."""
     if not numbers:
         return 0.0
-    
     sorted_numbers = sorted(numbers)
     n = len(sorted_numbers)
-    
-    if n % 2 == 0:
-        return (sorted_numbers[n // 2 - 1] + sorted_numbers[n // 2]) / 2
-    else:
-        return sorted_numbers[n // 2]
+    return (sorted_numbers[n // 2 - 1] + sorted_numbers[n // 2]) / 2 if n % 2 == 0 else sorted_numbers[n // 2]
