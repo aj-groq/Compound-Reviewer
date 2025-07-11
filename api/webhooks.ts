3da4e7cf5dcb82de0a32c8d30e18392e815bd795
Arnav Jaiswal <ajaiswal@groq.com>
2025-07-11 16:01:52 -0700

    refactor: update import path and enhance index.html with new design and features

--- api/webhooks.ts
+++ api/webhooks.ts
@@ -1,6 +1,6 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import { createNodeMiddleware, createProbot } from "probot";
-import { robot as app } from "../src/bot";
+import { robot as app } from "../src/bot.js";
 
 const probot = createProbot();
 const middleware = createNodeMiddleware(app, { probot, webhooksPath: '/api/webhooks' });
