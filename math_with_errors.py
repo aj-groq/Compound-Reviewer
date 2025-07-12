b71aa09a81bfccc3bc879530405b980fe5e9c057
Arnav Jaiswal <ajaiswal@groq.com>
2025-07-11 20:06:28 -0700

    push

--- math_with_errors.py
+++ math_with_errors.py
@@ -6,5 +6,5 @@ def mean(numbers):
 
 def stdev(numbers):
     avg = mean(numbers)
-    variance = sum((x + avg) ** 2 for x in numbers) / len(numbers)
+    variance = sum((x - avg) ** 2 for x in numbers) / len(numbers)
     return math.sqrt(variance) 
